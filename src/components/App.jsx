fc3d4c0146e519d7205d05cf6be483967507ff2d
alinabodnar1 <malina.bodnar@gmail.com>
2023-05-19 04:44:46 +0300

    update App on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './Form/Form';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
@@ -6,93 +6,77 @@ import { nanoid } from 'nanoid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default class App extends Component {
-  state = {
-    contacts: [],
-    filter: ''
-  };
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  })
+  
+  const [filter, setFilter] = useState('');
 
-
-  componentDidMount() {
+  useEffect(() => {
     const localContact = localStorage.getItem('contact');
-		if (localContact !== null) {
-      this.setState({
-        contacts: JSON.parse(localContact)
-      });
 
-		}
-	}
+    if (localContact !== null) {
+      setContacts(JSON.parse(localContact));
+      }
+  }, []);
 
-	componentDidUpdate(_, prevState) {
-		if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
-    } 
-    
-		if (prevState.contacts.length > this.state.contacts.length) {
-      toast.info("The contact is deleted successfully!");
-    }
-	}
-  addContact = ({ name, number}) => {
+
+  useEffect(() => {
+    localStorage.setItem('contact', JSON.stringify(contacts));
+  },[contacts])
+  
+  const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     }
-
-    if (this.state.contacts.some(contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim())) {
+    setContacts(state => {
+      return [
+        ...state,
+        contact
+      ]
+    });
+  
+    if (contacts.some(contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim())) {
       return toast.error(`The contact "${name}" is already in the contacts!`);
     } else {
       toast.success("The contact is created  successfully!");
     }
-
-    this.setState((prevState) => {
-      return {
-        contacts: [...prevState.contacts, contact],
-      }
-    })
-   }
-  
-  handleChange = e => {
-    this.setState({
-      filter: e.target.value,
-    });
   }
 
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter(contact => {
-      return (
-        contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-      );
-    });
+  const handleChange = ({target}) => {
+    setFilter(target.value);
+    };
+  
+
+  const filterContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim()));
   }
 
- deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
- }
-  
-  
-  render() {
-    
-    const { filter } = this.state;
-    return (
+   const deleteContact = id => {
+      setContacts(state => state.filter(contact => contact.id !== id));
+      toast.info("The contact is deleted successfully!");
+  }
+   
+  return (
       <div style={{marginLeft: "20px"}}>
         <h1>Phonebook</h1>
-        <Form addContact={this.addContact}  />
+        <Form addContact={addContact}  />
        
         <h2>Contacts</h2>
         <Filter
           value={filter}
-          onChange={this.handleChange} />
+          onChange={handleChange} />
         <ContactsList
-          contacts={this.filterContacts()}
-          onDelete={this.deleteContact} />
+          contacts={filterContacts()}
+          onDelete={deleteContact} />
         
         <ToastContainer autoClose ={3000} />
       </div>
     );
   }
-}
+
 
